e5ef8bc1d94975e92e8cde03388338f4c18e4733
Redginald Godeau <113888771+RedginaldGodeau@users.noreply.github.com>
2024-04-15 14:51:39 +0200

    fix: adding endline for errors

--- markdown2html.py
+++ markdown2html.py
@@ -3,21 +3,21 @@
 """
 _main convert markdown to html :)
 """
-
 from os.path import exists
 import sys
+
 if __name__ == "__main__":
 
     args = sys.argv
     if len(args) < 3:
-        sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         sys.exit(1)
 
     inputFile = args[1]
     outputFile = args[2]
 
     if not exists(inputFile):
-        sys.stderr.write("Missing " + inputFile)
+        sys.stderr.write("Missing " + inputFile + "\n")
         sys.exit(1)
 
     sys.exit(0)
